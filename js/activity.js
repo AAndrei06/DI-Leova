719d18711ca4f9dbba9d2c5f511da37961f7ab84
AAndrei06 <andreiarsenii90@gmail.com>
2023-08-24 15:37:12 +0300

    Big changes

---
+++ js/activity.js
@@ -0,0 +1,49 @@
+let planuri_lunare = document.getElementById("planuri-lunare");
+let planuri_anuale = document.getElementById("planuri-anuale");
+let ordine = document.getElementById("ordine");
+let buget = document.getElementById("buget");
+let sedinte = document.getElementById("sedinte");
+let decizii = document.getElementById("decizii");
+let programe = document.getElementById("programe");
+let note = document.getElementById("note");
+
+activityDb.onSnapshot((snapshot) => {
+    activities = snapshot.docs;
+
+    planuri_lunare.innerHTML = "";
+    planuri_anuale.innerHTML = "";
+    ordine.innerHTML = "";
+    buget.innerHTML = "";
+    sedinte.innerHTML = "";
+    decizii.innerHTML = "";
+    programe.innerHTML = "";
+    note.innerHTML = "";
+
+    for (let i = 0;i < activities.length;i++){
+        if (activities[i].data().category == 1){
+            planuri_lunare.innerHTML += `<a target = "_blank" href = "${activities[i].data().link}">${i+1}.${activities[i].data().title}</a>`
+        }
+        if (activities[i].data().category == 2){
+            planuri_anuale.innerHTML += `<a target = "_blank" href = "${activities[i].data().link}">${i+1}.${activities[i].data().title}</a>`
+        }
+        if (activities[i].data().category == 3){
+            ordine.innerHTML += `<a target = "_blank" href = "${activities[i].data().link}">${i+1}.${activities[i].data().title}</a>`
+        }
+        if (activities[i].data().category == 4){
+            buget.innerHTML += `<a target = "_blank" href = "${activities[i].data().link}">${i+1}.${activities[i].data().title}</a>`
+        }
+        if (activities[i].data().category == 5){
+           sedinte.innerHTML += `<a target = "_blank" href = "${activities[i].data().link}">${i+1}.${activities[i].data().title}</a>`
+        }
+        if (activities[i].data().category == 5){
+            decizii.innerHTML += `<a target = "_blank" href = "${activities[i].data().link}">${i+1}.${activities[i].data().title}</a>`
+        }
+        if (activities[i].data().category == 6){
+            programe.innerHTML += `<a target = "_blank" href = "${activities[i].data().link}">${i+1}.${activities[i].data().title}</a>`
+        }
+        if (activities[i].data().category == 7){
+            note.innerHTML += `<a target = "_blank" href = "${activities[i].data().link}">${i+1}.${activities[i].data().title}</a>`
+        }
+
+    }
+});
--- js/adminzone.js
+++ js/adminzone.js
@@ -4,13 +4,13 @@ let event_image = document.getElementById("link-image-event");
 let content_event = document.getElementById("content-of-event");
 let post_event_btn = document.getElementById("news-create-btn");
 
-post_event_btn.addEventListener("click",() => {
+post_event_btn.addEventListener("click", () => {
     let date = new Date();
 
     let event = {
-        image:event_image.value,
-        content:content_event.value,
-        date_posted:date.getTime(),
+        image: event_image.value,
+        content: content_event.value,
+        date_posted: date.getTime(),
     }
 
     eventsDb.add(event).then(() => {
@@ -25,13 +25,13 @@ let title_activity = document.getElementById("title-activity-input");
 let link_activity = document.getElementById("link-activity-input");
 let post_activity_btn = document.getElementById("activity-create-btn");
 
-post_activity_btn.addEventListener("click",() => {
+post_activity_btn.addEventListener("click", () => {
     let date1 = new Date();
     let activity = {
-        category:activity_category.value,
-        title:title_activity.value,
-        link:link_activity.value,
-        date_posted:date1.getTime(),
+        category: activity_category.value,
+        title: title_activity.value,
+        link: link_activity.value,
+        date_posted: date1.getTime(),
     }
 
     activityDb.add(activity).then(() => {
@@ -47,14 +47,14 @@ let title_report = document.getElementById("title-of-report");
 let link_report = document.getElementById("link-of-report");
 let post_report_btn = document.getElementById("report-create-btn");
 
-post_report_btn.addEventListener("click",() => {
+post_report_btn.addEventListener("click", () => {
     let date2 = new Date();
 
     let report = {
-        category:report_category.value,
-        title:title_report.value,
-        link:link_report.value,
-        date_posted:date2.getTime(),
+        category: report_category.value,
+        title: title_report.value,
+        link: link_report.value,
+        date_posted: date2.getTime(),
     }
 
     reportDb.add(report).then(() => {
@@ -63,4 +63,105 @@ post_report_btn.addEventListener("click",() => {
         title_report.value = "";
         link_report.value = "";
     });
+});
+
+let events_group = document.getElementById("event-delete-id-group");
+
+function compar(a, b) {
+    return b.data().date_posted - a.data().date_posted;
+}
+
+eventsDb.onSnapshot((snapshot) => {
+    let events = snapshot.docs;
+    events_group.innerHTML = "";
+    events.sort(compar);
+    for (let i = 0; i < events.length; i++) {
+        events_group.innerHTML += `
+        <div class="event-delete">
+            <div class="event-image">
+                <img src="${events[i].data().image}" width="100%" height="150">
+            </div>
+            <div class="content-event-delete">
+                <p>${events[i].data().content.slice(0, 200)}</p>
+            </div>
+            <button class="delete-event-btn" value = "${events[i].id}">Șterge Evenimentul</button>
+        </div>
+        `;
+    }
+
+    let btns_event = document.querySelectorAll(".delete-event-btn");
+
+    for (let i = 0; i < btns_event.length; i++) {
+        btns_event[i].addEventListener('click', function () {
+            if (confirm("Sunteți sigură că doriți să ștergeți acest eveniment?") == true) {
+                eventsDb.doc(btns_event[i].value).delete().then(() => {
+                    alert("Evenimentul a fost șters cu succes!!!");
+                });
+            }
+        });
+    }
+});
+
+let activities_group = document.getElementById("activity-delete");
+
+activityDb.onSnapshot((snapshot) => {
+    let activities = snapshot.docs;
+    activities_group.innerHTML = "";
+    activities.sort(compar);
+    for (let i = 0; i < activities.length; i++) {
+        activities_group.innerHTML += `
+        <div class = "activity-delete-object">
+            <div class = "content-activity-delete">
+                <a href = "${activities[i].data().link}">${activities[i].data().title}</a>
+            </div>
+            <div class = "btn-delete">
+            <button class = "activity-delete-btn" value = "${activities[i].id}" id = "activity-del-btn">Șterge Activitatea</button>
+            </div>
+        </div>
+        `;
+    }
+
+    let btns_activities = document.querySelectorAll("#activity-del-btn");
+
+    for (let i = 0; i < btns_activities.length; i++) {
+        btns_activities[i].addEventListener('click', function () {
+            if (confirm("Sunteți sigură că doriți să ștergeți acestă activitate?") == true) {
+                activityDb.doc(btns_activities[i].value).delete().then(() => {
+                    alert("Activitatea a fost ștearsă cu succes!!!");
+                });
+            }
+        });
+    }
+});
+
+let reports_group = document.getElementById("report-delete");
+
+reportDb.onSnapshot((snapshot) => {
+    let reports = snapshot.docs;
+    reports_group.innerHTML = "";
+    reports.sort(compar);
+    for (let i = 0; i < reports.length; i++) {
+        reports_group.innerHTML += `
+        <div class = "activity-delete-object">
+            <div class = "content-activity-delete">
+                <a href = "${reports[i].data().link}">${reports[i].data().title}</a>
+            </div>
+            <div class = "btn-delete">
+                <button class = "activity-delete-btn" value = "${reports[i].id}" id = "report-del-btn">Șterge Raportul</button>
+            </div>
+        </div>
+        `;
+    }
+
+    let btns_reports = document.querySelectorAll("#report-del-btn");
+
+    for (let i = 0; i < btns_reports.length; i++) {
+        btns_reports[i].addEventListener('click', function () {
+            if (confirm("Sunteți sigur că doriți să ștergeți acest raport?") == true) {
+                reportDb.doc(btns_reports[i].value).delete().then(() => {
+                    alert("Raportul a fost șters cu succes!!!");
+                });
+            }
+        });
+    }
 });
